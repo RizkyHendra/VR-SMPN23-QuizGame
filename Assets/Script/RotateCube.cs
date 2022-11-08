df4e897afd04c4f2fbbb4dfdf4547d9822737f4f
Rizky Hendra Saputra <71587872+RizkyHendra@users.noreply.github.com>
2022-11-08 08:44:35 +0700

    update

---
+++ Assets/Script/RotateCube.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCube : MonoBehaviour
+{
+    public float spinForce = 45f;
+    private bool isSpinning = false;
+  
+    void Update()
+    {
+        if(isSpinning)
+        {
+            transform.Rotate(0, spinForce * Time.deltaTime, 0);
+        }
+        else if(!isSpinning)
+        {
+            transform.Rotate(0, 0, 0);
+        }
+    }
+
+    public void ChangeSpin()
+    {
+        isSpinning = !isSpinning;
+    }
+}
